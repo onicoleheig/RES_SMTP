5b31487e93b38486bc8f53703974746440091331
lionelnanchen <lionel.nanchen@heig-vd.ch>
2018-04-17 10:41:03 +0200

    implement prank package

--- src/main/java/ch/heigvd/res/labs_smtp/model/prank/Prank.java
+++ src/main/java/ch/heigvd/res/labs_smtp/model/prank/Prank.java
@@ -1,4 +1,46 @@
 package ch.heigvd.res.labs_smtp.model.prank;
 
+import ch.heigvd.res.labs_smtp.model.mail.Group;
+import ch.heigvd.res.labs_smtp.model.mail.Message;
+import ch.heigvd.res.labs_smtp.model.mail.Person;
+
+import java.util.ArrayList;
+
 public class Prank {
+    Message msg;
+    Group group;
+
+    public Prank(Group groupe, Message msg) {
+        this.group = groupe;
+        this.msg = msg;
+    }
+
+    public Person getSender() {
+        return group.getSender();
+    }
+
+    public ArrayList<Person> getVictimeRecipients() {
+        return group.getMembers();
+    }
+
+    public String getSubject() {
+        return msg.getSubject();
+    }
+
+    public String getBody() {
+        return msg.getBody();
+    }
+
+    public ArrayList<Person> getCc() {
+        return group.getCc();
+    }
+
+    public ArrayList<Person> getBcc() {
+        return group.getBcc();
+    }
+
+    @Override
+    public String toString() {
+        return group + "\n" + msg;
+    }
 }
--- src/main/java/ch/heigvd/res/labs_smtp/model/prank/PrankGenerator.java
+++ src/main/java/ch/heigvd/res/labs_smtp/model/prank/PrankGenerator.java
@@ -1,4 +1,48 @@
 package ch.heigvd.res.labs_smtp.model.prank;
 
+import ch.heigvd.res.labs_smtp.model.mail.Group;
+import ch.heigvd.res.labs_smtp.model.mail.Person;
+import com.sun.tools.corba.se.idl.InvalidArgument;
+import ch.heigvd.res.labs_smtp.config.Config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PrankGenerator {
+
+    private Config config;
+    private ArrayList<Prank> pranks;
+
+    public PrankGenerator(Config config, int nbGroups) throws InvalidArgument {
+        this.config = config;
+
+        if(nbGroups < 1 || config.getNbGroups() < 1) {
+            throw new InvalidArgument("nbGroups can't be < 1");
+        }
+        pranks = new ArrayList<Prank>();
+
+        for(int i = 0; i < nbGroups; ++i) {
+            Collections.shuffle(config.getMessages());
+            int nbPersons = Math.max(2, config.getPersons().size() / nbGroups);
+            pranks.add(new Prank(groupeGenerator(nbPersons), config.getMessages().get(0)));
+        }
+    }
+
+    public ArrayList<Prank> getPranks() {
+        return pranks;
+    }
+
+    private Group groupeGenerator(int size) {
+        ArrayList<Person> receivers = new ArrayList<Person>();
+        ArrayList<Person> persons = config.getPersons();
+        Collections.shuffle(persons);
+        Person sender = persons.get(0);
+        if (size > persons.size()) {
+            size = persons.size();
+        }
+        for(int i = 1; i < size; ++i) {
+            receivers.add(config.getPersons().get(i));
+        }
+        return new Group(sender, receivers);
+    }
 }
