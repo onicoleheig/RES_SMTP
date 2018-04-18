6fccd674c35ba604319ae0036381ef310370b0cc
Olivier Nicole <olivier.nicolesoitnicoulaz@heig-vd.ch>
2018-04-18 08:52:52 +0200

    Edit SmtpClient, now working fine

--- src/main/java/ch/heigvd/res/labs_smtp/smtp/SmtpClient.java
+++ src/main/java/ch/heigvd/res/labs_smtp/smtp/SmtpClient.java
@@ -14,19 +14,17 @@ public class SmtpClient implements ISmtpClient {
     private PrintWriter printWriter;
     private BufferedReader bufferedReader;
 
-    private static final String LINE_RETURN = "\r\n";
     private static final String EHLO_CMD = "EHLO ";
     private static final String FROM_CMD = "MAIL FROM: ";
     private static final String RCPT_CMD = "RCPT TO: ";
-    private static final String DATA_CMD = "DATA" + LINE_RETURN;
+    private static final String DATA_CMD = "DATA";
     private static final String FROM_DATA = "From: ";
     private static final String TO_DATA = "To: ";
     private static final String CC_DATA = "Cc: ";
     private static final String BCC_DATA = "Bcc: ";
     private static final String SUBJECT_DATA = "Subject: ";
-    private static final String END_DATA = "." + LINE_RETURN;
+    private static final String END_DATA = ".";
     private static final String QUIT_CMD = "QUIT";
-
 
     public SmtpClient(Config config) {
         this.config = config;
@@ -36,13 +34,11 @@ public class SmtpClient implements ISmtpClient {
         socket = new Socket(config.getServer(), config.getPort());
         printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true );
         bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-        System.out.println(bufferedReader.readLine()); //read the welcome message and display it
+        System.out.println("Connected : " + bufferedReader.readLine()); //read the welcome message and display it
 
         //EHLO command
-        System.out.println("print : ");
-        printWriter.print(EHLO_CMD + "local" + LINE_RETURN);
+        printWriter.println(EHLO_CMD + "local");
         String line = bufferedReader.readLine();
-        System.out.println("line : " + line);
 
         //read all server responses following EHLO command
         while (line.startsWith("250-")) {
@@ -51,19 +47,19 @@ public class SmtpClient implements ISmtpClient {
         }
 
         //FROM command
-        printWriter.print(FROM_CMD + "<" + prank.getSender() + ">" + LINE_RETURN);
+        printWriter.println(FROM_CMD + "<" + prank.getSender() + ">");
         System.out.println(bufferedReader.readLine());
 
         //RCPT command
         for (Person person : prank.getVictimeRecipients()) {
-            printWriter.print(RCPT_CMD + "<" + person.getAddress() + ">" + LINE_RETURN);
+            printWriter.println(RCPT_CMD + "<" + person.getAddress() + ">");
             System.out.println(bufferedReader.readLine());
         }
 
         //if the prank include cc
         if (prank.getCc() != null) {
             for (Person person : prank.getCc()) {
-                printWriter.print(RCPT_CMD + "<" + person.getAddress() + ">" + LINE_RETURN);
+                printWriter.println(RCPT_CMD + "<" + person.getAddress() + ">");
                 System.out.println(bufferedReader.readLine());
             }
         }
@@ -71,18 +67,18 @@ public class SmtpClient implements ISmtpClient {
         //if the prank include bcc
         if (prank.getBcc() != null) {
             for (Person person : prank.getBcc()) {
-                printWriter.print(RCPT_CMD + "<" + person.getAddress() + ">" + LINE_RETURN);
+                printWriter.println(RCPT_CMD + "<" + person.getAddress() + ">" );
                 System.out.println(bufferedReader.readLine());
             }
         }
 
         //starting the DATA command
-        printWriter.print(DATA_CMD);
+        printWriter.println(DATA_CMD);
         line = bufferedReader.readLine();
         System.out.println(line);
 
         //from information
-        printWriter.print(FROM_DATA + prank.getSender().getAddress() + LINE_RETURN);
+        printWriter.println(FROM_DATA + prank.getSender().getAddress());
 
         //to information(s)
         String text = TO_DATA;
@@ -92,8 +88,7 @@ public class SmtpClient implements ISmtpClient {
             text += person.getAddress();
             comma = true;
         }
-        printWriter.print(text + LINE_RETURN);
-
+        printWriter.println(text);
 
         //cc information(s)
         if (prank.getCc() != null) {
@@ -104,7 +99,7 @@ public class SmtpClient implements ISmtpClient {
                 text += person.getAddress();
                 comma = true;
             }
-            printWriter.print(text + LINE_RETURN);
+            printWriter.println(text);
         }
 
         //cc information(s)
@@ -116,21 +111,20 @@ public class SmtpClient implements ISmtpClient {
                 text += person.getAddress();
                 comma = true;
             }
-            printWriter.print(text + LINE_RETURN);
+            printWriter.println(text);
         }
 
         //subject information
-        printWriter.print(SUBJECT_DATA + prank.getSubject() + LINE_RETURN + LINE_RETURN);
+        printWriter.println(SUBJECT_DATA + prank.getSubject());
 
         //body text
-        printWriter.print(prank.getBody() + LINE_RETURN);
-        printWriter.print(END_DATA);
+        printWriter.println(prank.getBody());
+        printWriter.println(END_DATA);
         line = bufferedReader.readLine();
         System.out.println(line);
-        printWriter.print(QUIT_CMD + LINE_RETURN);
+        printWriter.println(QUIT_CMD);
         printWriter.close();
         bufferedReader.close();
-        socket.close();
-    }
+        socket.close(); }
 }
 
